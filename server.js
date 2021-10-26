fdb4b88e7578a0be26e3f886eb289574bb4a808b
dotpegaso <dotpegaso@gmail.com>
2021-10-25 23:00:26 -0300

    rewrite

---
+++ server.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const express = require("express");
+const socketIO = require("socket.io");
+
+const PORT = process.env.PORT || 3000;
+const INDEX = "/index.html";
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Client connected");
+  socket.on("disconnect", () => console.log("Client disconnected"));
+
+  socket.on("diceroll", (diceroll) => {
+    console.log("diceroll", diceroll);
+    socket.broadcast.emit("diceroll", diceroll);
+  });
+});
+
+setInterval(() => io.emit("time", new Date().toTimeString()), 1000);
